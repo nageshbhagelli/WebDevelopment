24f2f6aae28cf6b5bcb621658910e6ef5320dfbe
nageshbhagelli <bhagellinagesh@gmail.com>
2025-03-17 19:02:49 +0530

    JS files added

---
+++ JS/DOM/app.js
@@ -0,0 +1,13 @@
+/*
+DOM Events: Events are signals that something has occurred. (user inputs I actions)
+    onclick (when an element is clicked)
+    onmouseenter (when mouse enters an element)
+
+    Event Listener: addEventListener
+    element.addEventListener( event, callback )
+
+this in Event Listeners:
+When 'this' is used in a callback of event handler of something, it refers to that something.
+
+
+*/
